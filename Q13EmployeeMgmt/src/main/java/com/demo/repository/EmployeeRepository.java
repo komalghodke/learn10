4139f2d08b38f4d5cdff9618d9a6d5a7a152b8e9
Komal Ghodke <68497113+komalghodke@users.noreply.github.com>
2026-07-09 22:16:08 +0530

    Delete Q13EmployeeMgmt directory

--- Q13EmployeeMgmt/src/main/java/com/demo/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.repository;
-
-import com.demo.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
